86e14e2269b30ea80af26fabaed679b69e3584a4
Yihui Xiong <yihui.xiong@hotmail.com>
2017-12-15 21:08:16 +0800

    fix the issue if hw:0 is not the default alsa control device

--- set_alsa_mixer.sh
+++ set_alsa_mixer.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-amixer sset  'ADC1 PGA gain' 100%
-amixer sset  'ADC2 PGA gain' 100%
-amixer sset  'ADC3 PGA gain' 100%
-amixer sset  'ADC4 PGA gain' 100%
-amixer sset  'ADC5 PGA gain' 100%
-amixer sset  'ADC6 PGA gain' 100%
-amixer sset  'ADC7 PGA gain' 100%
-amixer sset  'ADC8 PGA gain' 100%
+amixer -c 0 sset  'ADC1 PGA gain' 100%
+amixer -c 0 sset  'ADC2 PGA gain' 100%
+amixer -c 0 sset  'ADC3 PGA gain' 100%
+amixer -c 0 sset  'ADC4 PGA gain' 100%
+amixer -c 0 sset  'ADC5 PGA gain' 100%
+amixer -c 0 sset  'ADC6 PGA gain' 100%
+amixer -c 0 sset  'ADC7 PGA gain' 100%
+amixer -c 0 sset  'ADC8 PGA gain' 100%
 
-amixer sset  'CH1 volume' 64%
-amixer sset  'CH2 volume' 64%
-amixer sset  'CH3 volume' 64%
-amixer sset  'CH4 volume' 64%
-amixer sset  'CH5 volume' 64%
-amixer sset  'CH6 volume' 64%
-amixer sset  'CH7 volume' 64%
-amixer sset  'CH8 volume' 64%
+amixer -c 0 sset  'CH1 volume' 64%
+amixer -c 0 sset  'CH2 volume' 64%
+amixer -c 0 sset  'CH3 volume' 64%
+amixer -c 0 sset  'CH4 volume' 64%
+amixer -c 0 sset  'CH5 volume' 64%
+amixer -c 0 sset  'CH6 volume' 64%
+amixer -c 0 sset  'CH7 volume' 64%
+amixer -c 0 sset  'CH8 volume' 64%
 
-amixer cset numid=17 80%
+amixer -c 0 cset numid=17 80%
 
